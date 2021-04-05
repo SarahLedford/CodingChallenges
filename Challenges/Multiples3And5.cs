46601be9b3cb7ecc027c4aad18491b3ead90fbd2
SarahLedford <Sarah_Ledford@outlook.com>
2021-04-05 17:36:18 -0500

    Solved the problem to print the sum of all numbers divisible by 3 or 5 up to 1000

--- Challenges/Multiples3And5.cs
+++ Challenges/Multiples3And5.cs
@@ -10,6 +10,23 @@ namespace Challenges
     {
         static void Main(string[] args)
         {
+            //This is a variable to hold the sum to be displayed
+            int total = 0;
+
+            //This will loop through the numbers up to 1000
+            for (int i = 0; i < 1000; i++)
+            {
+                //This checks to see if the current number is evenly divisible by 3 or 5
+                //(if there is no remainder when i is divided by 3 or 5, it gets added to the total)
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    //This adds the numbers that meet the above criteria to the total
+                    total += i;
+                }
+            }
+
+            //Print out the total
+            Console.WriteLine(total);
         }
     }
 }
