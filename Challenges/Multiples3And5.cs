9e26cf409916ea60ac5ec3947519038984238b3c
SarahLedford <Sarah_Ledford@outlook.com>
2021-04-05 16:14:51 -0500

    Initial Commit

---
+++ Challenges/Multiples3And5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    class Multiples3And5
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
