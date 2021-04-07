87a3af16457935f6528da1fc891e68dc9cbef6f6
SarahLedford <Sarah_Ledford@outlook.com>
2021-04-07 09:18:14 -0500

    After futher inspection, I realized that my running total was not correct. Updated to show what I tried that didn't work, and to show a working method.

--- Challenges/FibonacciEvenSum.cs
+++ Challenges/FibonacciEvenSum.cs
@@ -10,37 +10,67 @@ namespace Challenges
     {
         static void Main(string[] args)
         {
-            //First number in Fib sequence
-            long num1 = 1;
+            #region First Try
+            //I had tried to refactor my fibonacci solution, but it does not come up with the correct answer of total being just over 4million
 
-            //Running total variable to keep track of sum
-            long total = 0;
+            ////First number in Fib sequence
+            //long num1 = 1;
 
-            //For loop iterates through the specified count and increments num2 by adding num1 onto it
-            for (long num2 = 1; num2 <= 4000000; num2 += num1)
+            ////Running total variable to keep track of sum
+            //long total = 0;
+
+            ////For loop iterates through the specified count and increments num2 by adding num1 onto it
+            //for (long num2 = 1; num2 <= 4000000;)
+            //{
+            //    //Printing out the values in testing to ensure Fib sequence is calculating correctly
+            //    Console.WriteLine($"{num1}\n{num2}");
+            //    if (num2 % 2 == 0)
+            //    {
+            //        //Printed out the even numbers to ensure they were being correctly selected
+            //        //Console.WriteLine($"Even: {num2}");
+            //        total += num2;
+            //    }
+            //    else if (num1 % 2 == 0)
+            //    {
+            //        //Printed out the even numbers to ensure they were being correctly selected
+            //        //Console.WriteLine($"Even: {num1}");
+            //        total += num1;
+            //    }
+
+            //    //num1 is changed before num2 to ensure an accurate addition
+            //    num1 += num2;
+            //    num2 += num1;
+
+            //}
+            //Console.WriteLine($"Total: {total}"); 
+            #endregion
+
+
+
+            int num2 = 2;
+            int num1 = 1;
+            int total = 0;
+
+            while (num2 <= 4000000)
             {
-                //Printing out the values in testing to ensure Fib sequence is calculating correctly
-                //Console.WriteLine($"{num1}\n{num2}");
+
+
+
                 if (num2 % 2 == 0)
                 {
-                    //Printed out the even numbers to ensure they were being correctly selected
-                    //Console.WriteLine($"Even: {num2}");
                     total += num2;
+                    //Console.WriteLine($"Even: {num2}");
                 }
-                else if (num1 % 2 == 0)
+                else if (num1 % 2 ==0)
                 {
-                    //Printed out the even numbers to ensure they were being correctly selected
-                    //Console.WriteLine($"Even: {num1}");
                     total += num1;
+                    //Console.WriteLine($"Even: {num1}");
                 }
-
-                //num1 is changed before num2 to ensure an accurate addition
-                num1 += num2;
-                
+                Console.WriteLine($"{num1}\n{num2}");
+                num1 = num1 + num2;
+                num2 += num1;
             }
-            Console.WriteLine($"total: {total}");
-
-
+            Console.WriteLine($"Total: {total}");
         }//end main
     }//end class
 }//end namespace
